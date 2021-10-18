d542430cba7dc84aa05382070a0da412a8232537
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2021-10-18 18:20:03 -0500

    add create function to app

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,17 @@
 class SongsController < ApplicationController
+  def index
+  all_songs = Songs.all
+  render json: all_songs.as_json
+  end
+
+  def create
+    new_song = Song.new(
+      title: params[:title],
+      artist: params[:artist],
+      album: params[:album],
+      year: params[:year]
+    )
+    new_song.save
+    render json: new_song.as_json
+  end
 end
